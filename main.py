bb5991daa7cdc778c7c17943f74785cb7937af44
Luiz Cesar <43610902+eliphaslevii@users.noreply.github.com>
2025-05-07 20:22:13 -0300

    main.py
    Update main,
    
    Remoção do logging,
    Adição do Remember-me
    Remoção do thread 
    Correção __main__/ __init__
    Troca do QVbox para QformLayout
    Troca de addWidget para addRow

--- main.py
+++ main.py
@@ -1,79 +1,84 @@
 import sys
+
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QLineEdit, QPushButton,
-    QVBoxLayout, QMessageBox
+    QVBoxLayout, QMessageBox, QFormLayout, QCheckBox
 )
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
-
-from auth_service import AuthService  # Importa a camada de serviço
-
-
-# THREAD DE AUTENTICAÇÃO
-class AuthThread(QThread):
-    result = pyqtSignal(bool)
-
-    def _init_(self, user, password):
-        super()._init_()
-        self.user = user
-        self.password = password
-
-    def run(self):
-        success = AuthService.validate_credentials(self.user, self.password)
-        self.result.emit(success)
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QSettings
 
 
 # TELA DE LOGIN
 class LoginWindow(QWidget):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle("Login")
         self.setGeometry(100, 100, 300, 150)
+        self.settings = QSettings("PI","App")
         self.init_ui()
+        self.triggerFocus()
+        self.loadCredentials()
 
     def init_ui(self):
-        layout = QVBoxLayout()
+        layout = QFormLayout()
 
+
+        #label/Input Username
         self.label_user = QLabel("Usuário:")
-        self.label_user.setAlignment(Qt.AlignCenter)
         self.input_user = QLineEdit()
-        layout.addWidget(self.label_user)
-        layout.addWidget(self.input_user)
+        #self.input_user.setMinimumHeight(50)
+        #self.input_user.setFont(QFont("Verdana",10,QFont.Bold))
 
+        #label/input Password
         self.label_pass = QLabel("Senha:")
-        self.label_pass.setAlignment(Qt.AlignCenter)
         self.input_pass = QLineEdit()
-        self.input_pass.setEchoMode(QLineEdit.Password)
-        layout.addWidget(self.label_pass)
-        layout.addWidget(self.input_pass)
 
+        self.input_pass.setEchoMode(QLineEdit.Password)
         self.login_button = QPushButton("Entrar")
-        self.login_button.clicked.connect(self.check_login)
-        layout.addWidget(self.login_button)
+        self.login_button.clicked.connect(self.login)
+
+        #checkBox Remember-me
+        self.rememberMe = QCheckBox()
+        self.rememberMeLabel = QLabel("Lembrar-me")
+
+
+        layout.addRow(self.label_user)
+        layout.addRow(self.input_user)
+        layout.addRow(self.label_pass)
+        layout.addRow(self.input_pass)
+        layout.addRow(self.rememberMe,self.rememberMeLabel)
+        layout.addRow(self.login_button)
 
         self.setLayout(layout)
 
-    def check_login(self):
-        user = self.input_user.text()
-        password = self.input_pass.text()
+    def login(self):
+        username = self.input_user.text()
+        password = self.input_user.text()
+        if username == 'admin' and password == 'admin':
+            if self.rememberMe.isChecked():
+                self.triggerRememberMe(username,password)
+            else:
+                self.input_pass.setText("")
+                self.input_user.setText("")
+                self.rememberMe.setChecked(False)
+                return
+    def triggerRememberMe(self,username,password):
+            if self.rememberMe.isChecked():
+                self.settings.setValue('username',username)
+                self.settings.setValue('password',password)
+                self.settings.setValue('remember',True)
 
-        self.login_button.setEnabled(False)
-        self.login_button.setText("Verificando...")
+    def loadCredentials(self):
+        remember = self.settings.value("remember", False, type=bool)
+        if remember:
+            self.input_user.setText(self.settings.value("username", ""))
+            self.input_pass.setText(self.settings.value("password", ""))
+            self.rememberMe.setChecked(True)
 
-        self.auth_thread = AuthThread(user, password)
-        self.auth_thread.result.connect(self.auth_finished)
-        self.auth_thread.start()
+    def triggerFocus(self):
+        self.input_user.setFocus()
 
-    def auth_finished(self, success):
-        if success:
-            QMessageBox.information(self, "Login", "Login bem-sucedido!")
-        else:
-            QMessageBox.critical(self, "Erro", "Usuário ou senha inválidos.")
-
-        self.login_button.setEnabled(True)
-        self.login_button.setText("Entrar")
-
-
-if _name_ == "_main_":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = LoginWindow()
     window.show()
